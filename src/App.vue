b9936f5908714fd315a18ae2858057b2003cb0b5
neathcmd <ksdf71464@gmail.com>
2025-06-04 15:47:13 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SideBar from "./components/SideBar.vue";
+</script>
+
+<template>
+  <SideBar />
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-900">
+    <ul>
+      <h1 class="text-white text-4xl">This is Side Bar</h1>
+    </ul>
+  </div>
+</template>
